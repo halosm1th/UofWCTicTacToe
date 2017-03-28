63e5ce40d38f9d1210ec1bb982aac849bf5f8f70
CrestonBarker <cresto2000@gmail.com>
2017-03-28 09:56:50 -0500

    Updated AI

--- TicTactoe/src/tictactoe/AI.java
+++ TicTactoe/src/tictactoe/AI.java
@@ -10,7 +10,7 @@ package tictactoe;
  * @author Halo
  */
 public class AI {
-    public int[][] gameBoard;
+    private int[][] gameBoard;
     public AI(int[][] gb){
         duplicateArray(gb, gameBoard);
     }
@@ -31,19 +31,32 @@ public class AI {
   
     private void findMove(
     ){ // Method for finding the best move to take by the computer. Searches 3(?) deep.
-      for (int x = 0; i < 3; i++) {  //cycle through 3 rows
+      for (int x = 0; x < 3; x++) {  //cycle through 3 rows
          for (int y = 0; y < 3; y++) { //cycle through 3 collumns
-           gameBoard[x][y] = 2;
-           //check win
-           gameBoard[x][y] = 0;
+           if(gameBoard[x][y] == 0){
+            gameBoard[x][y] = 2;
+            //check state, return win/loss/draw/cont 
+            gameBoard[x][y] = 0;
+            
+            //if win: end tree and make move
+            //if loss: do not look further in tree >how?
+            //if draw: look for other moves
+            //if cont: look for other moves
+            }
          }
        }
     }
   
   private void duplicateArray(int[][] inArray, // array to copy from
                               int[][] outArray // array to copy to
-                             ) {
-    for (int i = 0; i < inArray.
+    ) {
+    // Method duplicates a 2d int array from inArray to outArray
+      
+    inArray = outArray.clone();
+      
+    for (int i = 0; i < inArray.length; i++){
+        outArray[i] = inArray[i].clone();
+    } 
   }
 
 
