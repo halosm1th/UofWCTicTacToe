89bc3780130b2e2c4919aba7f16c344063bba675
CrestonBarker <cresto2000@gmail.com>
2017-03-30 10:18:42 -0500

    Ai file editied for new system

--- TicTactoe/src/tictactoe/AI.java
+++ TicTactoe/src/tictactoe/AI.java
@@ -5,14 +5,13 @@
  */
 package tictactoe;
 
+
 /**
  *
  * @author Halo
  */
 public class AI {
-    private int[][] gameBoard;
-    public AI(int[][] gb){
-        duplicateArray(gb, gameBoard);
+    public AI(){
     }
 
     public Coordinates getNextMove(){
@@ -21,40 +20,51 @@ public class AI {
         return returnCords;
     }
     
-    public boolean isValid(Coordinates c){
-        if(gameBoard[c.getX()][c.getY()] == 0){
+    public boolean isValid(char[][] gb, Coordinates c){
+        if(gb[c.getX()][c.getY()] == 0){
             return true;
         }else{
             return false;
         }
     }
   
-    private void findMove(
-    ){ // Method for finding the best move to take by the computer. Searches 3(?) deep.
+    private Coordinates findMove(char[][] gb
+    ){ // Method for finding the best move to take by the computer.
+        
+      char[][] gameBoard = new char[gb.length][gb[0].length];
+      
+      duplicateArray(gb, gameBoard);
+      
       for (int x = 0; x < 3; x++) {  //cycle through 3 rows
          for (int y = 0; y < 3; y++) { //cycle through 3 collumns
-           if(gameBoard[x][y] == 0){
-            gameBoard[x][y] = 2;
-            //check state, return win/draw/cont 
-            gameBoard[x][y] = 0;
+           Coordinates pos = new Coordinates(x, y);
+           if(isValid(gameBoard, pos)){
+            gameBoard[pos.getX()][pos.getY()] = 2;
+            //check state, return win/draw/cont;
             //if not win
-            //do cycle on node for player
+                //do cycle on node for player
             //if all are cont
-            //do cycle on node again
-            //if not win
+                //do cycle on node again
+                findMove(gb); //current state of board
+            //if lose
+                //end
            
-            
+            // for all conts, check for full board. if full stop
             //if win: end tree and make move
+            return(pos);
             //if loss: do not look further in tree >how?
             //if draw: look for other moves
             //if cont: look for other moves
+            
+            
             }
          }
        }
+      return(null);
     }
   
-  private void duplicateArray(int[][] inArray, // array to copy from
-                              int[][] outArray // array to copy to
+  private void duplicateArray(char[][] inArray, // array to copy from
+                              char[][] outArray // array to copy to
     ) {
     // Method duplicates a 2d int array from inArray to outArray
       
